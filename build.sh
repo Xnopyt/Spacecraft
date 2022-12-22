7dc131a57d620418659f37b13bc3fadbbfed381f
Xnopyt <billy@xnopyt.info>
2022-12-22 10:40:14 +0000

    build: clean up build.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
-rm -rf /spacecraft/sdloader/build
-cd /spacecraft/sdloader
-rm sdloader*
-rm payload.h
-make clean
-cd ../firmware
-rm payload.h
-rm firmware.bin
-rm firmware.elf
-rm -rf build
-cd ../bootloader_updater
-rm *.bin
-rm *.elf
-rm -rf build
-cd ../bootloader
-rm *.bin
-rm *.elf
-rm -rf build
 cd /spacecraft
 make clean
 make
-chown 1000:1000 /spacecraft/firmware/firmware.bin
+chown -R 1000:1000 /spacecraft/*
